b4457d33110acb0f3eeb697d342a90b6bdd85a80
Fragomo <francisco.gonzalezmorales66@gmail.com>
2024-06-18 17:01:29 +0100

    fix

--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,13 +1,8 @@
 const router = require("express").Router();
 
-<<<<<<< HEAD
 router.use('/user', require('./user.route'))
-=======
 router.use("/auth", require("./auth.route"));
-
 router.use('/area',require('./area.route'))
-
->>>>>>> 43c1cfe3a294f6bec15efea1ddaa8a594ec22770
 
 
 
