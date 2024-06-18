de0ebc2822fe67eaf2876ded6276aa760a90ca48
Francisreboot <fmrb@gmail.com>
2024-06-18 14:47:21 +0100

    Area model !

--- backend/db/relationships.js
+++ backend/db/relationships.js
@@ -1,5 +1,6 @@
 const Recipe = require("../api/models/recipes.model")
 const User = require("../api/models/user.model")
+const Area = require('../api/models/area.model')
 
 const initRelationships = () => {
   console.log('Chachi')
