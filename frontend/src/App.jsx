01796e71a05821de8d08b59d76b63f4ef6f0bce3
acozayda <traductora@gmail.com>
2024-06-19 16:54:29 +0100

    icons install

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,22 @@ import RecipesCard from './components/RecipesCard'
 import SearchRecipe from './components/SearchRecipe'
 import { useNavigate } from 'react-router-dom'
 import Signup from './components/RegisterForm'
+import { BeakerIcon, CheckBadgeIcon } from "@heroicons/react/24/outline";
 
 function App() {
   const navigate = useNavigate()
 
   return (
     <>
-      <button onClick={() => navigate('/register')}>Ir a Recipes</button>
-      <LoginForm/>
-      <SearchRecipe/>
+      <button onClick={() => navigate("/register")}>Ir a Recipes</button>
+      <LoginForm />
+      <SearchRecipe />
+      <div>
+        <BeakerIcon/>
+        <CheckBadgeIcon/>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
