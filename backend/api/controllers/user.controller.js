f259f4e61e3dcfb42242b4a1ebb3206f5aa2f97b
acozayda <traductora@gmail.com>
2024-06-19 10:21:48 +0100

    getoneuser

--- backend/api/controllers/user.controller.js
+++ backend/api/controllers/user.controller.js
@@ -12,6 +12,19 @@ async function getAllUsers(req, res) {
     res.status(500).send(error.message);
   }
 }
+async function getOneUser(req, res) {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      return res.status(200).json(user);
+    } else {
+      return res.status(404).send("User not found");
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
 async function createUser(req, res) {
   try {
     const user = await User.create(req.body);
--- backend/api/routes/user.route.js
+++ backend/api/routes/user.route.js
@@ -8,6 +8,7 @@ const {
 } = require("../controllers/user.controller");
 
 router.get("/", getAllUsers);
+router.get("/:id", getOneUser);
 router.post("/", createUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
