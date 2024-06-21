7a18c812bb14b3778ed31783f34c8c813b5843e0
Francisreboot <fmrb@gmail.com>
2024-06-21 15:31:38 +0100

    Header colocado

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Usbatlogout from '../components/Usbatlogout/Usbatlogout'
+import Header from '../components/HeaderComponent/Header'
 
 function Layout() {
   return (
     <div>
+      <Header/>
       <Usbatlogout/>
       <Outlet/>
     </div>
