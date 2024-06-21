2fde6aabbef935ea6c17b6553c0a9458f945a7bf
Fragomo <francisco.gonzalezmorales66@gmail.com>
2024-06-21 10:08:23 +0100

    header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,18 @@ import { useNavigate } from 'react-router-dom'
 import Signup from './components/RegisterForm'
 import { BeakerIcon, CheckBadgeIcon } from "@heroicons/react/24/outline";
 import UnregisterForm from './components/UnregisterForm/UnregisterForm'
+import Header from './components/HeaderComponent/Header'
+
 
 function App() {
   const navigate = useNavigate()
 
   return (
     <>
+      <Header/>
       <button onClick={() => navigate("/register")}>Ir a Recipes</button>
       <LoginForm />
+      <SearchRecipe/>
       <div id='btnContainer'>
         <button onClick={() => navigate("/myprofile")}>Share your Recipe</button>
         <button onClick={() => navigate("/recipes")}>Explore Recipes</button>
---
+++ frontend/src/components/HeaderComponent/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+function Header() {
+  
+    return (
+    
+    <div className='header'>
+        <li>Home</li>
+        <li>Profile</li>
+        <li>About us</li>
+
+    </div>
+  )
+}
+
+export default Header
