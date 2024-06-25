5d14806c7562f45e0feca4f76c4312829bce3729
acozayda <traductora@gmail.com>
2024-06-25 16:17:01 +0100

    Estilado footer header

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,54 +1,47 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <>
-        <div className='footer-spot'>
-            <ul id='Areas'>
-                <li className='area'>
-                    <h3>Areas</h3>
-                        <ul>
-                            <li>Asian</li>
-                            <li>Mediterránea</li>
-                            <li>Arabic</li>
-              
-                        </ul>
-                </li>
-
-                <li className='meal'>
-                    <h3>Types of meals</h3>
-                        <ul>
-                            <li>Breakfast</li>
-                            <li>Snacks</li>
-                            <li>Meal</li>
-                            <li>Dinner</li>
-                        </ul>
-                </li>
-
-                <li>
-                    <h3>Follow us</h3>
-                        <ul>
-                            <li>Facebook</li>
-                            <li>Instagram</li>
-                            <li>X</li>
-              
-                        </ul>
-                </li>
+      <div className="footer-spot">
+        <ul id="Areas">
+          <li className="area">
+            <h3>Areas</h3>
+            <ul>
+              <li>Asian</li>
+              <li>Mediterránea</li>
+              <li>Arabic</li>
             </ul>
-      
-        
+          </li>
 
-        <div>
-            <ul id='legal'>
-                <li>Cookies</li>
-                <li>Privacy</li>
-                <li>Terms & Conditions</li>
+          <li className="meal">
+            <h3>Types of meals</h3>
+            <ul>
+              <li>Breakfast</li>
+              <li>Snacks</li>
+              <li>Meal</li>
+              <li>Dinner</li>
             </ul>
-        </div>
-    </div>
+          </li>
+
+          <li>
+            <h3>Follow us</h3>
+            <ul>
+              <li>Facebook</li>
+              <li>Instagram</li>
+              <li>X</li>
+            </ul>
+          </li>
+          <ul id="legal">
+            <li>Cookies</li>
+            <li>Privacy</li>
+            <li>Terms & Conditions</li>
+          </ul>
+        </ul>
+      </div>
     </>
   );
 }
 
-export default Footer
+export default Footer;
