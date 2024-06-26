3b9c1f65ff96da568eb58b9428bc2cc5b5a170be
Francisreboot <fmrb@gmail.com>
2024-06-26 16:38:58 +0100

    he sido yo

--- frontend/src/pages/NewRecipes/NewRecipes.jsx
+++ frontend/src/pages/NewRecipes/NewRecipes.jsx
@@ -1,4 +1,3 @@
-
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import React, { useState } from "react";
@@ -12,104 +11,104 @@ export default function NewRecipes() {
   const [mealName, setMealName] = useState("");
   const [ingredients, setIngredients] = useState("");
   const [instruction, setInstruction] = useState("");
-  const [measure, setMeasure] = useState('')
+  const [measure, setMeasure] = useState("");
   const navigate = useNavigate();
   const [ingredientes, setIngredientes] = useState([]);
 
   async function handleClick(event) {
     event.preventDefault();
     await postRecipe(mealName, ingredientes, instruction);
-    
-    toast.success('Recipe created!')
-    
+
+    toast.success("Recipe created!");
+
     setTimeout(() => {
-        navigate('/myprofile')
-      }, 2000)
+      navigate("/myprofile");
+    }, 2000);
   }
 
   const renderList = () => {
     return ingredientes.map((item, index) => (
-      <li key={index}>{item.strIngredients} - {item.strMeasure}</li>
-    ))
+      <li key={index}>
+        {item.strIngredients} - {item.strMeasure}
+      </li>
+    ));
   };
-  console.log(ingredientes)
+  console.log(ingredientes);
 
   return (
     <>
-      <div className="title-newrecipes">
-        <h1>NewRecipes</h1>
-        <div className="input-section">
-          <input
-            type="text"
-            placeholder="Recipe Name"
-            value={mealName}
-            onChange={(event) => setMealName(event.target.value)}
-            required
-          />
-        </div>
-      </div>
-
-      <div className="new-recipes">
-        <Typography variant="h5">Ingredients</Typography>
-
-        <div className="input-section">
-          <input
-            placeholder="Ingredient - Example: Potatoes"
-            onChange={(event) => setIngredients(event.target.value)}
-            required
-            rows="5"
-            cols="50"
-          />
+      <div className="new-recipes-container">
+        <div className="title-newrecipes">
+          <h1>New Recipe</h1>
+          <div className="input-section">
+            <input
+              type="text"
+              placeholder="Recipe Name"
+              value={mealName}
+              onChange={(event) => setMealName(event.target.value)}
+              required
+            />
+          </div>
         </div>
 
-        <div className="input-section">
-          <input
-            id="measure"
-            placeholder="Measure - Example: 2kg"
-            onChange={(event) => setMeasure(event.target.value)}
-            required
-            rows="5"
-            cols="50"
-          />
+        <div className="new-recipes">
+
+          <div className="input-section">
+            <input
+              placeholder="Ingredient - Example: Potatoes"
+              onChange={(event) => setIngredients(event.target.value)}
+              required
+              rows="5"
+              cols="50"
+            />
+          </div>
+
+          <div className="input-section">
+            <input
+              id="measure"
+              placeholder="Measure - Example: 2kg"
+              onChange={(event) => setMeasure(event.target.value)}
+              required
+              rows="5"
+              cols="50"
+            />
+          </div>
+
+          <Fab
+            color="primary"
+            aria-label="add"
+            onClick={() =>
+              setIngredientes([
+                ...ingredientes,
+                { strIngredients: ingredients, strMeasure: measure },
+              ])
+            }
+          >
+            <AddIcon />
+          </Fab>
+        </div>
+        <div className="new-recipes">
+          <div className="input-textarea">
+            <Typography variant="h5">Instructions</Typography>
+            <textarea
+              id="measure"
+              placeholder="Example: BoilingWater"
+              onChange={(event) => setInstruction(event.target.value)}
+              required
+              rows="5"
+              cols="50"
+            />
+          </div>
+          <div className="preview">
+            <Typography variant="h5">Preview</Typography>
+            <span className="titleRecipe">{mealName}</span>
+            <ul>{renderList()}</ul>
+          </div>
         </div>
 
-        <Fab
-          color="primary"
-          aria-label="add"
-          onClick={() =>
-            setIngredientes([
-              ...ingredientes,
-              { strIngredients: ingredients, strMeasure: measure },
-            ])
-          }
-        >
-          <AddIcon />
-        </Fab>
-      </div>
-
-      <div className="aside">
-        <Typography variant="h5">Instructions</Typography>
-        <Typography variant="h5">Preview</Typography>
-      </div>
-      <div className="aside">
-        <div className="input-textarea">
-          <textarea
-            id="measure"
-            placeholder="Example: BoilingWater"
-            onChange={(event) => setInstruction(event.target.value)}
-            required
-            rows="5"
-            cols="50"
-          />
+        <div className="button-newrecipes">
+          <button onClick={handleClick}>Share your recipe</button>
         </div>
-
-        <div className="preview">
-          <span className="titleRecipe">{mealName}</span>
-          <ul>{renderList()}</ul>
-        </div>
-      </div>
-      <div className="button-newrecipes">
-        <button onClick={handleClick}>Share your recipe</button>
       </div>
     </>
   );
