6ee5675e11809dd6619894736215fc543c997289
Fragomo <francisco.gonzalezmorales66@gmail.com>
2024-06-26 09:08:06 +0100

    Update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,54 +4,32 @@ import "./Footer.css";
 function Footer() {
   return (
     <>
-        <div className='footer-spot'>
-            <ul id='Areas'>
-                <li className='area'>
-                    <p>Areas</p>
-                        <ul>
-                            <li>British</li>
-                            <li>Canadian</li>
-                            <li>Italian</li>
-                            <li>Etc.</li>
-              
-                        </ul>
-                </li>
-
-
-                <li>
-                    <p>Follow us</p>
-                        <ul>
-                            <li>Facebook</li>
-                            <li>Instagram</li>
-                            <li>X</li>
-              
-                        </ul>
-                </li>
-            </ul>
-
-          <li className="meal">
-            <h3>Types of meals</h3>
+      <div className="footer-spot">
+        <ul id="Areas">
+          <li className="area">
+            <p>Areas</p>
             <ul>
-              <li>Breakfast</li>
-              <li>Snacks</li>
-              <li>Meal</li>
-              <li>Dinner</li>
+              <li>British</li>
+              <li>Canadian</li>
+              <li>Italian</li>
+              <li>Etc.</li>
             </ul>
           </li>
 
           <li>
-            <h3>Follow us</h3>
+            <p>Follow us</p>
             <ul>
               <li>Facebook</li>
               <li>Instagram</li>
               <li>X</li>
             </ul>
           </li>
-          <ul id="legal">
-            <li>Cookies</li>
-            <li>Privacy</li>
-            <li>Terms & Conditions</li>
-          </ul>
+        </ul>
+        <ul id="legal">
+          <li>Cookies</li>
+          <li>Privacy</li>
+          <li>Terms & Conditions</li>
+        </ul>
       </div>
     </>
   );
